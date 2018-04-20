5093c731c5d4dadb7e1bdd251497c9399fcf4fcb
Joshua Horwitz <joshua.d.horwitz@gmail.com>
2018-04-19 21:03:24 -0400

    Client IO

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+  console.log('New user connected');
+});
+
+io.on('disconnect', () => {
+  console.log('User disconnected');
+});
+
+server.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
